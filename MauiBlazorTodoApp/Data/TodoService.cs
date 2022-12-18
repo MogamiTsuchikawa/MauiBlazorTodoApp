47d4f4785cf198deecceea64ed1260f2ba13b3f3
mogami <atsuyatsuchikawa@gmail.com>
2022-12-19 05:48:00 +0900

    完成（雑コミット）

--- MauiBlazorTodoApp/Data/TodoService.cs
+++ MauiBlazorTodoApp/Data/TodoService.cs
@@ -1,35 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using CommunityToolkit.Maui.Alerts;
 
 namespace MauiBlazorTodoApp.Data
 {
     public class TodoService
     {
         private static TodoService _instance;
-        public static TodoService Instance 
-        { 
+        public static TodoService Instance
+        {
             get
             {
-                if(_instance == null) 
+                if (_instance == null)
                     _instance = new TodoService();
                 return _instance;
-            } 
+            }
         }
 
-        public List<Todo> TodoList = new();
+        public ObservableCollection<Todo> TodoList = new();
         public TodoService()
         {
             string filePath = Path.Combine(
-                FileSystem.Current.AppDataDirectory, 
+                FileSystem.Current.AppDataDirectory,
                 "todolist.json");
             if (File.Exists(filePath))
             {
                 string json = File.ReadAllText(filePath);
-                TodoList = JsonSerializer.Deserialize<List<Todo>>(json);
+                TodoList = new(JsonSerializer.Deserialize<List<Todo>>(json));
+                DateTime lastTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 23, 59, 59);
+                TodoList.Where(t => 
+                    t.Limit is null ? 
+                    false : 
+                    ((DateTime)t.Limit).CompareTo(lastTime) == -1 && 
+                    ((DateTime)t.Limit).CompareTo(lastTime.AddDays(-1)) == 1).ToList().ForEach(t =>
+                    {
+                        Toast.Make($"本日期限:{t.Title}", CommunityToolkit.Maui.Core.ToastDuration.Long,14).Show();
+                    });
             }
         }
 
